d8660c59cf24c3d945cb0dc0aba35b007913310c
Abdullah Kurkcu <ak4728@nyu.edu>
2021-02-11 23:24:58 -0700

    upd utils get_market_exchange symbol_url

--- utils.py
+++ utils.py
@@ -6,11 +6,11 @@ symbol_url = "https://symbol-search.tradingview.com/symbol_search/?text={}"
 def get_market_exchange(ticker):
     r = requests.get(symbol_url.format(ticker))
     for el in r.json():
-        if el['exchange'] in ['NYSE', 'NASDAQ', 'BINANCE', 'BITTREX']:
-            if el['symbol'] == ticker.upper():
+        if el['symbol'] == ticker.upper():
+            if el['exchange'] in ['NYSE', 'NASDAQ', 'BINANCE', 'BITTREX']:
                 response = el
-    screener = response['type']
-    exc = response['exchange']
-    if exc in ['NYSE', 'NASDAQ']:
+    if response['exchange'] in ['NYSE', 'NASDAQ']:
         screener = "america"
-    return(screener, exc)
+    else:
+        screener = "crypto"
+    return(screener, response['exchange'])
