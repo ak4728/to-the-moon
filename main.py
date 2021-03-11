3c44b79f2a2ccf2eec93ad449cf0b273c5644e6d
Abdullah Kurkcu <ak4728@nyu.edu>
2021-03-11 16:11:14 -0700

    add help statements for each function

--- main.py
+++ main.py
@@ -108,49 +108,46 @@ async def get_ticker(ticker="TSLA", interval=Interval.INTERVAL_4_HOURS, screener
 
 class MarketCommands(commands.Cog):
     """
-    Functions regarding the market, ticker price, and indicators. [] = required, {} = optional"
     !ticker [stock_name] {indicator_name}
+        > !ticker TSLA
+        > !ticker TSLA osc
     """
     @commands.command(
         ticker="Retrieves indicators for a given stock or cryptocoin. [] = required, {} = optional",
-        brief="Retrieves stock indicators. \n  !ticker [ixic] \n  !ticker ixic ma"
+        brief="Retrieves stock indicators including moving avgs and oscillators."
     )
     async def ticker(self, ctx, *args):
         async with ctx.channel.typing():
             await asyncio.sleep(0.01)
             embed = discord.Embed(color=json_data['ticker_color'])
-            embed.set_thumbnail(url="https://cdn2.iconfinder.com/data/icons/mix-color-5/100/Mix_color_5__info-512.png")
-            try:
-                ticker_name = args[0]
-                stock = await get_ticker(ticker_name, intervals[selected])
-                analyzed = stock.get_analysis()
-                embed.add_field(name="Stock", value="${}".format(analyzed.symbol.upper()), inline=False)
-                embed.add_field(name="Price", value="${}".format(get_ticker_price(ticker_name.upper())), inline=False)
-                embed.add_field(name="Recommendation", value="{}".format(analyzed.summary['RECOMMENDATION']),
-                                inline=False)
-                embed.add_field(name="Buy", value="{}".format(analyzed.summary['BUY']), inline=True)
-                embed.add_field(name="Sell", value="{}".format(analyzed.summary['SELL']), inline=True)
-                embed.add_field(name="Neutral", value="{}".format(analyzed.summary['NEUTRAL']), inline=True)
-                if len(args) > 1:
-                    if args[1] == "ma":
-                        await get_ma_osc(embed, stock, "ma")
-                    if args[1] == "osc":
-                        await get_ma_osc(embed, stock, "osc")
-            except Exception as e:
-                print(e)
+            if len(args) == 0:
+                embed.set_thumbnail(
+                    url="https://cdn2.iconfinder.com/data/icons/mix-color-5/100/Mix_color_5__info-512.png")
                 embed.add_field(name="Missing Arguments",
-                                value="\n > !ticker [ticker_name] [indicator_name] \n > !ticker ixic \n > !ticker ixic osc",
+                                value="```!ticker [ticker_name] [indicator_name] \n!ticker ixic \n!ticker ixic osc```",
                                 inline=True)
                 embed.set_footer(text="Indicator names, Oscillators/Moving Averages, [osc, ma] are optional. ")
-
+            else:
+                embed.set_thumbnail(url=json_data['image'])
+                for arg in args:
+                    ticker_name = arg
+                    stock = await get_ticker(ticker_name, intervals[selected])
+                    analyzed = stock.get_analysis()
+                    embed.add_field(name="```${}```".format(analyzed.symbol.upper()), value="${}".format(get_ticker_price(ticker_name.upper())), inline=False)
+                    embed.add_field(name="Recommendation", value="{}".format(analyzed.summary['RECOMMENDATION']),
+                                    inline=False)
+                    embed.add_field(name="Buy", value="{}".format(analyzed.summary['BUY']), inline=True)
+                    embed.add_field(name="Sell", value="{}".format(analyzed.summary['SELL']), inline=True)
+                    embed.add_field(name="Neutral", value="{}".format(analyzed.summary['NEUTRAL']), inline=True)
+                    if args[-1] == "ma":
+                        await get_ma_osc(embed, stock, "ma")
+                    if args[-1] == "osc":
+                        await get_ma_osc(embed, stock, "osc")
         await ctx.channel.send(embed=embed)
 
 
 class Configuration(commands.Cog):
     """
-    Configuration functions.
-    [] = required, {} = optional
-
     !interval [interval]
         > !interval 1-minute
 
@@ -161,8 +158,8 @@ class Configuration(commands.Cog):
     """
 
     @commands.command(
-        watchlist="Adds or removes a stock or shows the watchlist",
-        brief="Watchlist functions. \n  !watchlist \n  !watchlist add TSLA \n  !watchlist remove TSLA"
+        watchlist="Adds or removes a stock or shows the watchlist.",
+        brief="See or change stocks in the watchlist."
     )
     async def watchlist(self, ctx, *args):
         """
@@ -177,10 +174,10 @@ class Configuration(commands.Cog):
         embed.set_thumbnail(url="https://dl3.pushbulletusercontent.com/v3GAmZvD2FtVZ2SK9IY1YppLLZYoJuca/bookmark.png")
         if len(args) > 1:
             fun = args[0]
-            stock = args[1]
-            embed.add_field(name="Watchlist is updated. Function: {}.".format(fun.upper()),
-                            value="${}".format(stock.upper()), inline=True)
-            await watchlist(str(stock), str(fun))
+            for stock in args[1:]:
+                embed.add_field(name="Watchlist is updated. Function: {}.".format(fun.upper()),
+                                value="${}".format(stock.upper()), inline=True)
+                await watchlist(str(stock), str(fun))
         else:
             with open("watchlist.txt", "r") as f:
                 tickers = list(json.load(f).keys())
@@ -190,18 +187,22 @@ class Configuration(commands.Cog):
         await ctx.channel.send(embed=embed)
 
     @commands.command(
-        interval="Shows or changes the interval",
-        brief="Interval functions. \n  !interval \n  !interval 15-minutes"
+        interval="Shows or changes the interval.",
+        brief="See or change candle intervals."
     )
     async def interval(self, ctx, *args):
         """
+        See or change candle intervals.
+
         Existing interval
-            !interval
+        !interval
+
         Set interval
-            !interval 15-minutes
+        !interval 15-minutes
+
         Intervals
-            1-minute, 5-minutes, 15-minutes, 1-hour,
-            4-hours, 1-day, 1-week, 1-month
+        1-minute, 5-minutes, 15-minutes, 1-hour,
+        4-hours, 1-day, 1-week, 1-month
 
         """
         print(intervals[selected])
@@ -217,31 +218,77 @@ class Configuration(commands.Cog):
 
 
 class SocialMedia(commands.Cog):
+    """
+    !sentiment [ticker_name]
+        > !sentiment TSLA
 
+    !reddit {#_of_posts}
+        > !reddit
+        > !reddit 100
+    """
     @commands.command(
-        interval="Shows or changes the interval",
-        brief="Interval functions. \n  !interval \n  !interval 15-minutes"
+        sentiment="Sentiment analysis of a given ticker on Twitter.",
+        brief="Sentiment analysis of a given ticker on Twitter."
     )
     async def sentiment(self, ctx, *args):
-        async with message.channel.typing():
+        """
+        Sentiment analysis of a given ticker on Twitter.
+        !sentiment TSLA
+        """
+        async with ctx.channel.typing():
             await asyncio.sleep(0.1)
-            try:
-                stock = message.content.split('!sentiment ')[1].split(" ")[0]
-                tweets, pos, neg, neu = get_sentiment(stock, dollar=True)
-                count = len(tweets['id'])
-                pos_rate = pos / len(tweets['id'])
-                tweet_image = "https://www.shareicon.net/data/512x512/2015/09/04/95557_twitter_512x512.png"
-                embed = discord.Embed(color=1146986)
-                embed.set_thumbnail(url=tweet_image)
-                embed.add_field(name="{} Tweets within the last hour".format(stock.upper()),
-                                value='> Positive Tweets: {}\n> Negative Tweets: {}\n> Neutral Tweets: {}\n> Positivity Rate: {:2.2%}'.format(
-                                    pos,
-                                    neg,
-                                    neu,
+            embed = discord.Embed(color=1146986)
+            if len(args) == 0:
+                embed.add_field(name="Missing Arguments",
+                                value="```!sentiment [ticker_name] \n!sentiment THETA```",
+                                inline=True)
+                msg_image = "https://cdn2.iconfinder.com/data/icons/mix-color-5/100/Mix_color_5__info-512.png"
+            else:
+                msg_image = "https://www.shareicon.net/data/512x512/2015/09/04/95557_twitter_512x512.png"
+                for arg in args:
+                    stock = arg
+                    tweets, pos, neg, neu = get_sentiment(stock, dollar=True)
+                    count = len(tweets['id'])
+                    pos_rate = pos / len(tweets['id'])
+
+                    embed.add_field(name="{} Tweets within the last hour".format(stock.upper()),
+                                    value='> Positive Tweets: {}\n> Negative Tweets: {}\n> Neutral Tweets: {}\n> Positivity Rate: {:2.2%}'.format(
+                                        pos,
+                                        neg,
+                                        neu,
                                     pos_rate), inline=False)
-            except Exception as e:
-                print("Exception in Sentiment {}".format(e))
-            await message.channel.send(embed=embed)
+            embed.set_thumbnail(url=msg_image)
+            await ctx.channel.send(embed=embed)
+
+    @commands.command(
+        reddit="# of stock mentions in r/wallstreetbets",
+        brief="# of stock mentions in r/wallstreetbets. default limit: 100"
+    )
+    async def reddit(self, ctx, *args):
+        """
+        # of stock mentions in r/wallstreetbets. default limit: 100
+        !reddit 100
+        """
+        msg_image = "https://www.redditinc.com/assets/images/site/reddit-logo.png"
+        embed = discord.Embed(color=11027200)
+        embed.set_thumbnail(url=msg_image)
+        async with ctx.channel.typing():
+            await asyncio.sleep(0.01)
+            try:
+                sr_limit = args[0]
+            except:
+                sr_limit = 100
+            df = await get_reddit_stocks(sr_limit)
+            text = ""
+            for x, y in df.iloc[0:10].iterrows():
+                text = text + "{}:{} \n".format(y[0], y[2])
+            text = "```"+text+"```"
+            embed.add_field(name="Top Reddit Stocks - Limit:{}".format(sr_limit), value='{}'.format(text),
+                            inline=False)
+
+            await ctx.channel.send(embed=embed)
+
+
 
 @tasks.loop(seconds=900.0)
 async def signalAlarm():
@@ -308,5 +355,6 @@ async def on_ready():
 
 client.add_cog(MarketCommands())
 client.add_cog(Configuration())
+client.add_cog(SocialMedia())
 signalAlarm.start()
 client.run(json_data['discord_token'])
